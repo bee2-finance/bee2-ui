a27fa94351ec7f70bc066417818da26ec5655280
xiaotian <952822399@qq.com>
2020-09-02 16:07:10 +0800

    cli: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@typescript-eslint/parser'
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     quotes: [2, 'single'], // https://eslint.org/docs/2.0.0/rules/quotes
     semi: [2, 'never'], // https://eslint.org/docs/2.0.0/rules/semi
-  }
-};
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript'
+  ]
+}
